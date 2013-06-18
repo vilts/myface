fe01a079222cba58feeb9e46ac8b1d115bc9831d
Viljo Marrandi <viljo.marrandi@skype.net>
2013-06-18 22:01:57 +0300

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'vilts-myface'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'vilts'
+maintainer_email 'astas@sharplasers.com'
 license          'All rights reserved'
 description      'Installs/Configures vilts-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
